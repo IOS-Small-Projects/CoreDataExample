c1f08dfc633ac5f48e41537960009445a54b006b
Vandana <vinodkakad@Mac-mini.local>
2016-09-14 18:30:48 +0530

    Example of core data

--- CoreDataExample/ViewController.swift
+++ CoreDataExample/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,53 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet var userNam: UITextField!
 
+    @IBOutlet var password: UITextField!
+    
+    @IBAction func loadBtnClicked(sender: AnyObject) {
+        let appDel: AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        
+        let request = NSFetchRequest(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        var results:NSArray = []
+        do{
+             results = try context.executeFetchRequest(request)
+        }
+        catch{
+            print("Error while reading data")
+        }
+        
+        if results.count > 0 {
+            print(results)
+        }else{
+            print("Error while reading data")
+        }
+        
+        
+    }
+    
+    @IBAction func saveBtnClicked(sender: AnyObject){
+        
+        let appDel: AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        
+        let newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context)
+        
+        newUser.setValue("Test Username", forKey: "username")
+        newUser.setValue("Test Password", forKey: "password")
+        
+        do{
+            try context.save();
+        }
+        catch {
+            print("Error while saving data")
+        }
+        
+        print(newUser)
+    }
 }
 
